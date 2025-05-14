e7b72172f7609a465201fa238f82101503279f63
Xavier <admin@54xavier.cn>
2025-05-14 12:02:06 +0800

    feat: 添加可设置的友好设备名称(nickName)配置项

--- tools/utils.js
+++ tools/utils.js
@@ -15,6 +15,10 @@ const schema = {
     type: "string",
     default: "Electron-hiprint",
   },
+  nickName: {
+    type: "string",
+    default: "",
+  },
   openAtLogin: {
     type: "boolean",
     default: true,
@@ -164,6 +168,7 @@ function emitClientInfo(socket) {
       ipv6: _address.ipv6(), // ipv6 地址
       clientUrl: `http://${_address.ip()}:${store.get("port") || 17521}`, // 客户端地址
       machineId: machineIdSync({ original: true }), // 客户端唯一id
+      nickName: store.get("nickName"), // 客户端昵称
     });
   });
 }
